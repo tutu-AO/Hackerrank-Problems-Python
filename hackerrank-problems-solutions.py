b07b68eded3c791270b3638d5b07e6d3d9bc15a4
tutu-AO <98568883+tutu-AO@users.noreply.github.com>
2024-01-23 20:10:12 -0500

    Update hackerrank-problems-solutions.py

--- hackerrank-problems-solutions.py
+++ hackerrank-problems-solutions.py
@@ -26,3 +26,19 @@ def maxDiff(string):
 
     return ans
 print(maxDiff("abab"))
+
+'''
+2. Here is about to introduce a new kind of number system. Where instead of 10 digits there is 20, from a to t, all in small. 
+Now a means 1, b means 2 and t means 20, thenn aa means 21. Now for  a new number you have to print the decimal form it.
+Note that the letters in the input string will be lower case and from a to t.
+Examples:
+Sample input 1: e     -> Sample Output: 5
+Sample  Input 2: ac   -> Sample Output: 23
+'''
+def newNumSys(string, lenStr, i):
+    if lenStr < 0:
+        return 0
+    return (ord(string[lenStr]) - ord("a") + 1) * (20**i) + newNumSys(string, lenStr - 1, i + 1)
+
+s="aba"
+print(newNumSys(s, len(s)-1, 0))
